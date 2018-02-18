aed98852ba3b604e5dd3011ebe38309eeec38870
ronthecookie <ronthecookie0101@gmail.com>
2018-02-18 22:15:25 +0200

    Inital Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+const r = require("rethinkdbdash")({db: "urlshortener"})
+app.listen(port, () => console.log(`Listening on port ${port}`));
+app.disable("x-powered-by");
+app.get("/", (req, res) => {
+    res.send("Hello! This is my private URL Shortener.");
+});
+app.get("/:id", async (req, res) => {
+    let url = await r.table("urls").get(req.params.id).run();
+    if (!url) res.sendStatus(404);
+    res.redirect(url.url);
+});
