f57d5c889679e07b596de6db2af5572d3a45ce6a
RONTheCookie <ronthecookie0101@gmail.com>
2019-01-02 17:27:49 +0200

    Implement DB_ADDR env

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
-const r = require("rethinkdbdash")({db: "urlshortener"});
+const dbOpts = {
+    db: process.env.db || `urlshortener`,
+};
+if (process.env.DB_ADDR) dbOpts.servers = [{host: process.env.DB_ADDR.split(":")[0], port: parseInt(process.env.DB_ADDR.split(":")[1],10)}]
+const r = require(`rethinkdbdash`)(dbOpts); // Connect to RethinkDB
 app.use(require("helmet")());
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.disable("x-powered-by");
